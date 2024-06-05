36ab39de8add4afda06d4215db3ed5508223c1ec
natangoatoso <natan.almeida03@gmail.com>
2024-06-05 14:40:16 -0300

    contatos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 const toggleButton = document.getElementById('toggleButton');
+var cont = document.getElementById('contatos')
 
 function toggleDarkMode() {
      document.body.classList.toggle('dark-mode');
@@ -8,17 +9,20 @@ function toggleDarkMode() {
 
      if (document.body.classList.contains('dark-mode')) {
           icon.textContent = "light_mode";
+          cont.style.filter = "invert()";
      } else {
           icon.textContent = "dark_mode";
+          cont.style.filter = "none";
      }
 }
 
 toggleButton.addEventListener('click', toggleDarkMode);
 const icon = document.getElementById('modeicon');
+var cont = document.getElementById('contatos')
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
      document.body.classList.add('dark-mode');
-     icon.textContent = "light_mode"
+     icon.textContent = "light_mode";
 } else {
      document.body.classList.add('light-mode');
      icon.textContent = "dark_mode"
